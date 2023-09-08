5beb7ec630fc69c9965535f6b6ee65bc5ef0556b
subaqueousReach <139270668+subaqueousReach@users.noreply.github.com>
2023-09-08 12:29:09 -0700

    Modified Contact Section
    Made it into a sticky button

---
+++ js/portfolio.js
@@ -0,0 +1,7 @@
+function openForm() { // Shows Contact Form
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() { // Hides Contact Form
+    document.getElementById("myForm").style.display = "none";
+}
